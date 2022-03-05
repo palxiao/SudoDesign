5ecfdc7c1b161fa98502aaa1d1b950b12a304082
ShawnPhang <palxiao@vip.qq.com>
2022-03-05 22:27:49 +0800

    build: auto publish

--- src/views/Draw.vue
+++ src/views/Draw.vue
@@ -132,7 +132,7 @@ export default defineComponent({
     compressImages(widgets: any) {
       for (const item of widgets) {
         if (item.imgUrl && item.imgUrl.split('.')[item.imgUrl.split('.').length - 1] === 'png') {
-          item.imgUrl += '@small'
+          item.imgUrl.split('.')[1] === 'palxp' && (item.imgUrl += '@small')
         }
       }
     },
